bf37122ac141cc0b6a2cbbb6e3a54619da259690
Mukengeshayi <desiremukengeshayi27@gmail.com>
2023-12-07 07:06:58 -0800

    construct notion

--- Person.php
+++ Person.php
@@ -3,6 +3,17 @@ class Person{
     var $firstName;
     var $lastName;
     var $age;
+
+    public function __construct($firstName, $lastName, $age)
+    {
+        echo $this->firstName=$firstName;
+        echo  $this->firstName=$lastName;
+        echo $this->firstName=$age;
+    }
+
 }
-$honore= new Person;
-echo $honore->lastName. PHP_EOL;
+$honore= new Person('Desire', 'muk', 10 );
+$toto= new Person('Henock', 'nkoy',20);
+
+
+
